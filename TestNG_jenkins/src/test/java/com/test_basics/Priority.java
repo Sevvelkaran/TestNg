42c71bfda4fec12e7fa8d11f3962f844c7535917
Sevvelkaran <2k21it50@kiot.ac.in>
2025-03-21 12:36:28 +0530

    piriority

---
+++ TestNG_jenkins/src/test/java/com/test_basics/Priority.java
@@ -0,0 +1,26 @@
+package com.test_basics;
+
+import org.testng.annotations.Test;
+
+public class Priority {
+  @Test
+  public void f() {
+	  System.out.println("1");
+  }
+  @Test
+  public void q() {
+	  System.out.println("2");
+  }
+  @Test
+  public void w() {
+	  System.out.println("3");
+  }
+  @Test
+  public void r() {
+	  System.out.println("4");
+  }
+  @Test
+  public void t() {
+	  System.out.println("5");
+  }
+}
