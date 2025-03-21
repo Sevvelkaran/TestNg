ed3f57c761b73689b6f2898906c307c5e0da4efa
Sevvelkaran <2k21it50@kiot.ac.in>
2025-03-21 13:06:14 +0530

    commit

--- TestNG_jenkins/src/test/java/com/test_basics/Priority.java
+++ TestNG_jenkins/src/test/java/com/test_basics/Priority.java
@@ -1,26 +1,30 @@
 package com.test_basics;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
-public class Priority {
-  @Test
-  public void f() {
-	  System.out.println("1");
-  }
-  @Test
-  public void q() {
-	  System.out.println("2");
-  }
-  @Test
-  public void w() {
-	  System.out.println("3");
-  }
-  @Test
-  public void r() {
-	  System.out.println("4");
-  }
-  @Test
-  public void t() {
-	  System.out.println("5");
-  }
-}
+	
+	public class Priority {
+	    WebDriver driver;
+
+
+	    @Test
+	    public void f() {
+	        driver = new ChromeDriver();  // Initialize WebDriver once
+	        driver.get("https://google.com");	    }
+
+	    @Test(dependsOnMethods = {"f"})
+	    public void f1() {
+	        driver.findElement(By.name("q")).sendKeys("Depends on" + Keys.ENTER);
+	    }
+	    @Test(dependsOnMethods = {"f1"})
+	    public void f2() {
+	        
+	            driver.quit();
+	        
+	    }
+	}
