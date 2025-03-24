e8df2fa9c0f7f44cd225d74e8ba51b91a8503129
Sevvelkaran <2k21it50@kiot.ac.in>
2025-03-24 16:08:17 +0530

    commit

--- TestNG_jenkins/src/test/java/com/pages/DashBoardPage.java
+++ TestNG_jenkins/src/test/java/com/pages/DashBoardPage.java
@@ -5,16 +5,16 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class DashBoardPage {
-    WebDriver driver;
+public class DashBoardPage extends BasePage{
+    public DashBoardPage(WebDriver driver) {
+		super(driver);
+		
+	}
 
     @FindBy(xpath = "//h6[text()='Dashboard']")
     WebElement homePageUserName;
 
-    public DashBoardPage(WebDriver driver) {
-        this.driver = driver;
-        PageFactory.initElements(driver, this);
-    }
+
 
     public String getHomePagetext() {
         return homePageUserName.getText();
