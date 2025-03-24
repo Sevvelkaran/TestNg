ad320d90f591fdfb56c444fbe3d2f7b4a6083daa
Sevvelkaran <2k21it50@kiot.ac.in>
2025-03-24 23:00:36 +0530

    Junit+TestNG

---
+++ TestNG_jenkins/src/test/java/TEST/JUnitTest.java
@@ -0,0 +1,12 @@
+package TEST;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class JUnitTest {
+    @Test
+    public void testJUnitExample() {
+        System.out.println("Running JUnit Test");
+        assertTrue(true);
+    }
+}
---
+++ TestNG_jenkins/src/test/java/TEST/TestNGTest.java
@@ -0,0 +1,13 @@
+package TEST;
+
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class TestNGTest {
+    @Test
+    public void testTestNGExample() {
+        System.out.println("Running TestNG Test");
+        Assert.assertTrue(true);
+    }
+}
