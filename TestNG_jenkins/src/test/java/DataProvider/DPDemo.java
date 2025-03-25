fd0172b2344f3129a9a7dd5a98e2b0ce59c59a4f
Sevvelkaran <2k21it50@kiot.ac.in>
2025-03-25 13:27:36 +0530

    jenkins try

--- TestNG_jenkins/src/test/java/DataProvider/DPDemo.java
+++ TestNG_jenkins/src/test/java/DataProvider/DPDemo.java
@@ -2,13 +2,14 @@ package DataProvider;
 
 import org.testng.annotations.DataProvider;
 
-
 public class DPDemo {
-	@DataProvider(name="testData")
+    
+	@DataProvider(name="testData", parallel = true)
 	public Object[][] dataProvFunc(){
 		return new Object[][] {
-			{"Selenium"},
-			{"TestNG"},
-			{"Automation"}};
-		}
+			{"Automation Tester", "2-5 years"},
+			{"Performance Tester", "3+ years"},
+			{"DevOps", "5+ years"} };
+
+    }
 }
--- TestNG_jenkins/src/test/java/DataProvider/DataProviderDemo.java
+++ TestNG_jenkins/src/test/java/DataProvider/DataProviderDemo.java
@@ -1,40 +1,77 @@
 package DataProvider;
 
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
 
-public class DataProviderDemo  extends DPDemo{
-	WebDriver driver;
-//	@DataProvider(name = "testData")
+public class DataProviderDemo {
+	private static final ThreadLocal<WebDriver> driverThreadLocal = new ThreadLocal<>();
+//	@DataProvider(name="testData")
 //	public Object[][] dataProvFunc(){
-//		return new Object[][] {{"Selenium"}, {"TestNG"}};
-	//}
-	@BeforeMethod
-	public void setup() {
-		System.out.println("Start the test");
-		driver = new FirefoxDriver();
+//		return new Object[][] {{"Selenium"},{"TestNG"}};
+//	}
+//  @Test(dataProvider = "testData", dataProviderClass = DPDemo.class)
+	@Test(dataProvider = "testData", dataProviderClass = DPDemo.class)
+  public void search(String Keyword,String Experience) { 
+		WebDriver driver = getDriver();
+//	  WebElement txtBox=driver.findElement(By.id("sb_form_q"));
+//	  txtBox.sendKeys(Keyword);
+//	  System.out.println("Keyword entered is: "+Keyword);
+//	  txtBox.sendKeys(Keys.ENTER);
+	  WebElement txtBox = driver.findElement(By.id("sb_form_q"));
+	  txtBox.sendKeys(Keyword + " " + Experience);
+	  System.out.println("Keyword entered is: " + Keyword + " " + Experience);
+	  txtBox.sendKeys(Keys.ENTER);
+	  WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20)); 
+      wait.until(ExpectedConditions.titleContains(Keyword));
+	    String searchResultPageTitle = driver.getTitle();
+	    Assert.assertTrue(searchResultPageTitle.contains(Keyword), "Search result page title does not contain the keyword: " + Keyword);
+	  System.out.println("Search result is displayed.");
+	  
+  }
+  @BeforeMethod
+  public void setup() {
+	  System.out.println("Start the test");
+	  WebDriver driver = getDriver();
+	  ChromeOptions option = new ChromeOptions();
+		option.addArguments("--headless");
+
+		driver = new ChromeDriver(option);
 		driver.get("https://www.bing.com/");
 		driver.manage().window().maximize();
-	}
-  @Test(dataProvider = "testData")
-  public void search(String keyWord) {
-	  WebElement txtbox = driver.findElement(By.id("sb_form_q"));
-	  txtbox.sendKeys(keyWord);
-	  System.out.println("KeyWord Entred  : "+keyWord);
-	  txtbox.sendKeys(Keys.ENTER);
-	  System.out.println("Search result is Displayed");
-	  System.out.println("--------------------------");
-
+		setDriver(driver);
   }
+
   @AfterMethod
-  public void Teardown() {
+  public void afterMethod() {
+	  WebDriver driver = getDriver();
 	  driver.quit();
+	  removeDriver();
+	  System.out.println("End the test");
+  }
+  private void setDriver(WebDriver driver) {
+      driverThreadLocal.set(driver);
+  }
+
+  private WebDriver getDriver() {
+      return driverThreadLocal.get();
+  }
+
+  private void removeDriver() {
+      driverThreadLocal.remove();
   }
 }
